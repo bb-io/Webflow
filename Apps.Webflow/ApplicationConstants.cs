3a24d233e229322548a0ba0eac8914c1627d3f95
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-22 13:50:10 +0300

    add sites:write scope to ApplicationConstants

--- Apps.Webflow/ApplicationConstants.cs
+++ Apps.Webflow/ApplicationConstants.cs
@@ -4,5 +4,5 @@ public class ApplicationConstants
 {
     public const string ClientId = "#{WEBFLOW_CLIENT_ID}#";
     public const string ClientSecret = "#{WEBFLOW_SECRET}#";
-    public const string Scope = "sites:read cms:read cms:write pages:read pages:write components:read components:write";
+    public const string Scope = "sites:read sites:write cms:read cms:write pages:read pages:write components:read components:write";
 }
