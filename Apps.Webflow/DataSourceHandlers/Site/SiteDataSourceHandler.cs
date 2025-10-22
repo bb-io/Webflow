123b210324ac665706ace03a713979d6aa3b5dcc
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-22 18:57:48 +0300

    fix SiteDataSourceHandler throwing NullReferenceException

--- Apps.Webflow/DataSourceHandlers/Site/SiteDataSourceHandler.cs
+++ Apps.Webflow/DataSourceHandlers/Site/SiteDataSourceHandler.cs
@@ -1,6 +1,6 @@
 using RestSharp;
 using Apps.Webflow.Invocables;
-using Apps.Webflow.Models.Response.Site;
+using Apps.Webflow.Models.Entities;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
@@ -11,7 +11,7 @@ public class SiteDataSourceHandler(InvocationContext invocationContext) : Webflo
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
     {
         var request = new RestRequest("sites", Method.Get);
-        var response = await Client.ExecuteWithErrorHandling<SearchSitesResponse>(request);
+        var response = await Client.ExecuteWithErrorHandling<SiteEntitiesList>(request);
         return response.Sites.Select(x => new DataSourceItem(x.Id, x.DisplayName)).ToList();
     }
 }
