db302fd41a56209e33f200277396182b4b23c5fd
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-31 15:35:36 +0200

    fix CollectionDataSourceHandler not loading collection items for single oauth connection

--- Apps.Webflow/DataSourceHandlers/Collection/CollectionDataSourceHandler.cs
+++ Apps.Webflow/DataSourceHandlers/Collection/CollectionDataSourceHandler.cs
@@ -1,10 +1,10 @@
-using Apps.Webflow.Models.Request;
+using Apps.Webflow.Models.Request.Collection;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.Webflow.DataSourceHandlers.Collection;
 
-public class CollectionDataSourceHandler(InvocationContext invocationContext, [ActionParameter] SiteRequest request) 
+public class CollectionDataSourceHandler(InvocationContext invocationContext, [ActionParameter] CollectionRequest request)
     : BaseCollectionDataSourceHandler(invocationContext, request.SiteId)
 {
 }
--- Apps.Webflow/Models/Request/Collection/CollectionRequest.cs
+++ Apps.Webflow/Models/Request/Collection/CollectionRequest.cs
@@ -1,11 +1,16 @@
 using Apps.Webflow.DataSourceHandlers.Collection;
+using Apps.Webflow.DataSourceHandlers.Site;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Webflow.Models.Request.Collection;
 
 public class CollectionRequest
-{    
+{
+    [Display("Site ID")]
+    [DataSource(typeof(SiteDataSourceHandler))]
+    public string? SiteId { get; set; }
+
     [Display("Collection ID")]
     [DataSource(typeof(CollectionDataSourceHandler))]
     public string CollectionId { get; set; }
