db39fc8ad44c3fa82c08b3346d7725d115e4173a
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-03 10:21:10 +0200

    refactor CollectionActions: update GetCollection return type, add missing SiteRequest inputs, add missing async/awaits, delete unused collection classes

--- Apps.Webflow/Models/Entities/FullCollectionEntity.cs
+++
@@ -1,9 +0,0 @@
-using Blackbird.Applications.Sdk.Common;
-
-namespace Apps.Webflow.Models.Entities;
-
-public class FullCollectionEntity : CollectionEntity
-{
-    [Display("Collection items")]
-    public IEnumerable<CollectionItemEntity> CollectionItems { get; set; }
-}
