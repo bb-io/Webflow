af8312b604c38059d7b4d2c737c7e90c85c54dc5
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-10 16:56:17 +0200

    refactor WebhookList to fetch cmsLocaleId for every collection item request

--- Apps.Webflow/Webhooks/Models/Response/CollectionItemPublishedResponse.cs
+++ Apps.Webflow/Webhooks/Models/Response/CollectionItemPublishedResponse.cs
@@ -4,6 +4,9 @@ namespace Apps.Webflow.Webhooks.Models.Response;
 
 public class CollectionItemPublishedResponse
 {
+    // You can always use Items.First() safely.
+    // Although it's a list, the API will always return one element in it.
+    // For some reason :/
     [JsonProperty("items")]
     public List<CollectionWebhookResponse> Items { get; set; }
 }
--- Apps.Webflow/Webhooks/WebhookList.cs
+++ Apps.Webflow/Webhooks/WebhookList.cs
@@ -29,8 +29,7 @@ public class WebhookList(InvocationContext invocationContext) : WebflowInvocable
 
     [Webhook("On page created", typeof(PageCreatedWebhookHandler),
         Description = "Triggers when specific page was created")]
-    public Task<WebhookResponse<PageCreatedResponse>> OnPageCreated(
-        WebhookRequest webhookRequest,
+    public Task<WebhookResponse<PageCreatedResponse>> OnPageCreated(WebhookRequest webhookRequest,
         [WebhookParameter] PageWebhookRequest input)
     {
         var data = webhookRequest.GetPayload<PageCreatedResponse>();
@@ -71,8 +70,7 @@ public class WebhookList(InvocationContext invocationContext) : WebflowInvocable
 
     [Webhook("On page metadata updated", typeof(PageMetadataUpdatedWebhookHandler),
         Description = "Triggers when specific page metadata was updated")]
-    public Task<WebhookResponse<PageUpdatedResponse>> OnPageMetadataUpdated(
-        WebhookRequest webhookRequest,
+    public Task<WebhookResponse<PageUpdatedResponse>> OnPageMetadataUpdated(WebhookRequest webhookRequest,
         [WebhookParameter] PageWebhookRequest input)
     {
         var data = webhookRequest.GetPayload<PageUpdatedResponse>();
@@ -97,7 +95,55 @@ public class WebhookList(InvocationContext invocationContext) : WebflowInvocable
         [WebhookParameter] CollectionItemWebhookRequest input)
     {
         var data = webhookRequest.GetPayload<CollectionWebhookResponse>();
+        return await FilterCollectionItems(data, input);
+    }
 
+    [Webhook("On collection item updated", typeof(CollectionItemChangedWebhookHandler),
+        Description = "Triggers when specific collection item was changed")]
+    public async Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemChanged(WebhookRequest webhookRequest,
+        [WebhookParameter] CollectionItemWebhookRequest input)
+    {
+        var data = webhookRequest.GetPayload<CollectionWebhookResponse>();
+        return await FilterCollectionItems(data, input);
+    }
+
+    [Webhook("On collection item deleted", typeof(CollectionItemDeletedWebhookHandler),
+        Description = "Triggers when specific collection item was deleted")]
+    public async Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemDeleted(WebhookRequest webhookRequest,
+        [WebhookParameter] CollectionItemWebhookRequest input)
+    {
+        var data = webhookRequest.GetPayload<CollectionWebhookResponse>();
+        return await FilterCollectionItems(data, input);
+    }
+
+    [Webhook("On collection item published", typeof(CollectionItemPublishedWebhookHandler),
+        Description = "Triggers when specific collection item was published")]
+    public async Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemPublished(WebhookRequest webhookRequest,
+        [WebhookParameter] CollectionItemWebhookRequest input)
+    {
+        var data = webhookRequest.GetPayload<CollectionItemPublishedResponse>();
+        return await FilterCollectionItems(data.Items.First(), input);
+    }
+
+    [Webhook("On collection item unpublished", typeof(CollectionItemUnpublishedWebhookHandler),
+        Description = "Triggers when specific collection item was unpublished")]
+    public async Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemUnpublished(WebhookRequest webhookRequest,
+        [WebhookParameter] CollectionItemWebhookRequest input)
+    {
+        var data = webhookRequest.GetPayload<CollectionWebhookResponse>();
+        return await FilterCollectionItems(data, input);
+    }
+
+    private static bool DoesNotMatch(string? filter, string? value)
+    {
+        return !string.IsNullOrWhiteSpace(filter) &&
+               (string.IsNullOrEmpty(value) || !value.Contains(filter, StringComparison.OrdinalIgnoreCase));
+    }
+
+    private async Task<WebhookResponse<CollectionItemResponse>> FilterCollectionItems(
+        CollectionWebhookResponse data, 
+        CollectionItemWebhookRequest input)
+    {
         if (!string.IsNullOrEmpty(input.Locale))
         {
             var cmsLocaleId = await LocaleHelper.GetCmsLocaleId(input.Locale, Client.GetSiteId(input.SiteId), Client);
@@ -113,92 +159,6 @@ public class WebhookList(InvocationContext invocationContext) : WebflowInvocable
             HttpResponseMessage = null,
             Result = data
         };
-    }
-
-    [Webhook("On collection item updated", typeof(CollectionItemChangedWebhookHandler),
-        Description = "Triggers when specific collection item was changed")]
-    public Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemChanged(WebhookRequest webhookRequest,
-        [WebhookParameter] CollectionItemWebhookRequest input)
-    {
-        var data = webhookRequest.GetPayload<CollectionWebhookResponse>();
-
-        if (input.Locale != null && data.FieldData["_locale"]!.ToString() != input.Locale)
-            return Preflight<CollectionItemResponse>();
-
-        if (input.CollectionId != null && data.CollectionId != input.CollectionId)
-            return Preflight<CollectionItemResponse>();
-
-        return Task.FromResult<WebhookResponse<CollectionItemResponse>>(new()
-        {
-            HttpResponseMessage = null,
-            Result = data
-        });
-    }
-
-    [Webhook("On collection item deleted", typeof(CollectionItemDeletedWebhookHandler),
-        Description = "Triggers when specific collection item was deleted")]
-    public Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemDeleted(WebhookRequest webhookRequest,
-        [WebhookParameter] CollectionItemWebhookRequest input)
-    {
-        var data = webhookRequest.GetPayload<CollectionWebhookResponse>();
-
-        if (input.CollectionId != null && data.CollectionId != input.CollectionId)
-            return Preflight<CollectionItemResponse>();
-
-        if (input.Locale != null && data.FieldData["_locale"]!.ToString() != input.Locale)
-            return Preflight<CollectionItemResponse>();
-
-        return Task.FromResult<WebhookResponse<CollectionItemResponse>>(new()
-        {
-            HttpResponseMessage = null,
-            Result = data
-        });
-    }
-
-    [Webhook("On collection item published", typeof(CollectionItemPublishedWebhookHandler),
-        Description = "Triggers when specific collection item was published")]
-    public Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemPublished(WebhookRequest webhookRequest,
-        [WebhookParameter] CollectionItemWebhookRequest input)
-    {
-        var data = webhookRequest.GetPayload<CollectionItemPublishedResponse>();
-
-        if (input.Locale != null && data.Items.First().FieldData["_locale"]!.ToString() != input.Locale)
-            return Preflight<CollectionItemResponse>();
-
-        if (input.CollectionId != null && data.Items.First().CollectionId != input.CollectionId)
-            return Preflight<CollectionItemResponse>();
-
-        return Task.FromResult<WebhookResponse<CollectionItemResponse>>(new()
-        {
-            HttpResponseMessage = null,
-            Result = data.Items.First()
-        });
-    }
-
-    [Webhook("On collection item unpublished", typeof(CollectionItemUnpublishedWebhookHandler),
-        Description = "Triggers when specific collection item was unpublished")]
-    public Task<WebhookResponse<CollectionItemResponse>> OnCollectionItemUnpublished(WebhookRequest webhookRequest,
-        [WebhookParameter] CollectionItemWebhookRequest input)
-    {
-        var data = webhookRequest.GetPayload<CollectionWebhookResponse>();
-
-        if (input.Locale != null && data.FieldData["_locale"]!.ToString() != input.Locale)
-            return Preflight<CollectionItemResponse>();
-
-        if (input.CollectionId != null && data.CollectionId != input.CollectionId)
-            return Preflight<CollectionItemResponse>();
-
-        return Task.FromResult<WebhookResponse<CollectionItemResponse>>(new()
-        {
-            HttpResponseMessage = null,
-            Result = data
-        });
-    }
-
-    private static bool DoesNotMatch(string? filter, string? value)
-    {
-        return !string.IsNullOrWhiteSpace(filter) &&
-               (string.IsNullOrEmpty(value) || !value.Contains(filter, StringComparison.OrdinalIgnoreCase));
     }
 
     private static Task<WebhookResponse<T>> Preflight<T>() where T : class
