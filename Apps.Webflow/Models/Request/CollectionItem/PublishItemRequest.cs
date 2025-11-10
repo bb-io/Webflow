a0a6883ae9718f52441c31c6ac565801db0e9ef9
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-10 10:35:39 +0200

    rename PublishItemRequest.CmsLocales display name

--- Apps.Webflow/Models/Request/CollectionItem/PublishItemRequest.cs
+++ Apps.Webflow/Models/Request/CollectionItem/PublishItemRequest.cs
@@ -16,7 +16,7 @@ public class PublishItemRequest
     [DataSource(typeof(PublishCollectionItemDataSourceHandler))]
     public string CollectionItemId { get; set; }
 
-    [Display("Locale")]
+    [Display("Locales")]
     [DataSource(typeof(SiteLocaleDataSourceHandler))]
     public IEnumerable<string>? CmsLocales { get; set; }
 }
