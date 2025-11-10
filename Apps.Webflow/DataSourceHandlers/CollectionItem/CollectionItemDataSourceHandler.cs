8d9e829e99be2ab075e39f166a17c4d745aad95e
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-10 13:04:49 +0200

    refactor CollectionItemDataSourceHandler

--- Apps.Webflow/DataSourceHandlers/CollectionItem/CollectionItemDataSourceHandler.cs
+++ Apps.Webflow/DataSourceHandlers/CollectionItem/CollectionItemDataSourceHandler.cs
@@ -1,42 +1,29 @@
+using RestSharp;
+using Apps.Webflow.Helper;
 using Apps.Webflow.Invocables;
 using Apps.Webflow.Models.Entities;
-using Apps.Webflow.Models.Request.CollectionItem;
-using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
-using Blackbird.Applications.Sdk.Utils.Extensions.String;
-using RestSharp;
 
 namespace Apps.Webflow.DataSourceHandlers.CollectionItem;
 
-public class CollectionItemDataSourceHandler : WebflowInvocable, IAsyncDataSourceHandler
+public class CollectionItemDataSourceHandler(InvocationContext invocationContext, string siteId, string collectionId, string? cmsLocale) 
+    : WebflowInvocable(invocationContext), IAsyncDataSourceItemHandler
 {
-    private CollectionItemRequest Request { get; }
-
-    public CollectionItemDataSourceHandler(InvocationContext invocationContext,
-        [ActionParameter] CollectionItemRequest request) : base(invocationContext)
+    public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
     {
-        Request = request;
-    }
+        if (string.IsNullOrWhiteSpace(collectionId))
+            throw new PluginMisconfigurationException("You need to specify Collection ID first");
 
-    public async Task<Dictionary<string, string>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
-    {
-        if (string.IsNullOrWhiteSpace(Request.CollectionId))
-            throw new("You need to specify Collection ID first");
+        var request = new RestRequest($"collections/{collectionId}/items", Method.Get);
+        if (!string.IsNullOrEmpty(cmsLocale))
+        {
+            var cmsLocaleId = await LocaleHelper.GetCmsLocaleId(cmsLocale, siteId, Client);
+            request.AddQueryParameter("cmsLocaleId", cmsLocaleId);
+        }
 
-        var endpoint = $"collections/{Request.CollectionId}/items";
-
-        endpoint = string.IsNullOrEmpty(Request.CmsLocale)
-            ? endpoint
-            : endpoint.SetQueryParameter("cmsLocaleIds", Request.CmsLocale);
-        var request = new RestRequest(endpoint, Method.Get);
         var response = await Client.Paginate<CollectionItemEntity>(request);
-
-        return response
-            .Where(x => context.SearchString is null ||
-                        x.Name.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
-            .OrderByDescending(x => x.LastUpdated)
-            .Take(50)
-            .ToDictionary(x => x.Id, x => x.Name);
+        return response.Select(ci => new DataSourceItem(ci.Id, ci.Name));
     }
 }
