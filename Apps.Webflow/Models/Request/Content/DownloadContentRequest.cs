9dc86eda3aae0ecdea7917283f092caa95808621
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-13 10:36:04 +0200

    update DownloadContentRequest display names

--- Apps.Webflow/Models/Request/Content/DownloadContentRequest.cs
+++ Apps.Webflow/Models/Request/Content/DownloadContentRequest.cs
@@ -27,9 +27,9 @@ public class DownloadContentRequest : IDownloadContentInput
     [StaticDataSource(typeof(DownloadFileFormatHandler))]
     public string? FileFormat { get; set; } = "text/html";
 
-    [Display("Include slug", Description = "Include slug in output file. Default is false")]
+    [Display("Include slug (only for pages)", Description = "Include slug in output file. Default is false")]
     public bool? IncludeSlug { get; set; } = false;
 
-    [Display("Include file metadata", Description = "Include file metadata in output file. Default is false")]
+    [Display("Include file metadata (only for pages)", Description = "Include file metadata in output file. Default is false")]
     public bool? IncludeMetadata { get; set; } = false;
 }
